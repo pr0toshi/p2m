d5a239c1a535dedfc036462e07b1bbacfd3ff5f3
pr0toshi <46229158+pr0toshi@users.noreply.github.com>
2020-02-06 03:09:03 +0000

    Update Token.sol

--- Token.sol
+++ Token.sol
@@ -54,14 +54,6 @@ contract IERC721 is IERC165 {
   function transferFrom(address from, address to, uint256 tokenId) public;
   function safeTransferFrom(address from, address to, uint256 tokenId)
     public;
-
-  function safeTransferFrom(
-    address from,
-    address to,
-    uint256 tokenId,
-    bytes memory data
-  )
-    public;
 }
 
 /**
